04130dfd5d3dd10387cae73a9d1c7eb6555e1534
mittalvaibhav1 <53215737+mittalvaibhav1@users.noreply.github.com>
2020-08-30 01:36:53 +0530

    Added AboutComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import {COMMENTS}  from '../shared/comments';
 import {PROMOTIONS}  from '../shared/promotions';
 import {LEADERS}  from '../shared/leaders';
+import About from './AboutComponent';
 class Main extends Component {
   constructor(props){
     super(props);
@@ -44,6 +45,7 @@ class Main extends Component {
             <Route path="/home" component={HomePage} />
             <Route exact path='/contactus' component={Contact} />
             <Route path="/menu/:dishId" component={DishWithId} />
+            <Route exact path="/aboutus" component={()=><About leaders={this.state.leaders}/>} />
             <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
             <Redirect to="/home" />
           </Switch>
