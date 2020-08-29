6a9ea281d7bd28cf4a01494b9bacb7cc9923c881
mittalvaibhav1 <53215737+mittalvaibhav1@users.noreply.github.com>
2020-08-30 00:02:56 +0530

    Contact and DishDetail fin

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React , { Component } from 'react';
-import {Card,CardBody,CardTitle,CardImg,CardText} from 'reactstrap';
-
+import {Card,CardBody,CardTitle,CardImg,CardText,BreadcrumbItem,Breadcrumb} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
   function  RenderDish({dish}){
         return(
@@ -39,8 +39,18 @@ import {Card,CardBody,CardTitle,CardImg,CardText} from 'reactstrap';
             return (
                 <div className="container">
                     <div className = "row">
-                      <RenderDish dish={props.dish} />
-                      <RenderComments comments={props.dish.comments} />
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
                 )
