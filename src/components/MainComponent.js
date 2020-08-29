13767a530b622963f933faf5fd87efb60b78c0fc
mittalvaibhav1 <53215737+mittalvaibhav1@users.noreply.github.com>
2020-08-29 18:25:35 +0530

    Updated MainComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,17 +6,28 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {Switch,Route,Redirect} from 'react-router-dom';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import {COMMENTS}  from '../shared/comments'
+import {PROMOTIONS}  from '../shared/promotions'
+import {LEADERS}  from '../shared/leaders'
 class Main extends Component {
   constructor(props){
     super(props);
     this.state ={
       dishes : DISHES,
+      comments: COMMENTS,
+      promotions : PROMOTIONS,
+      leaders: LEADERS
     };
   }
   render(){
-    const HomePage =() => {
+    const HomePage = () => {
       return(
-        <Home/>
+        <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       )
     }
     return (
@@ -24,6 +35,7 @@ class Main extends Component {
         <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
+            <Route exact path='/contactus' component={Contact} />
             <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
             <Redirect to="/home" />
           </Switch>
