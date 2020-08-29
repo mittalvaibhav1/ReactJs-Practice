c3e07719c21d8afbd4ba827a8479de4c55315c7d
mittalvaibhav1 <53215737+mittalvaibhav1@users.noreply.github.com>
2020-08-29 20:21:11 +0530

    HomeComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,9 +7,9 @@ import Footer from './FooterComponent';
 import {Switch,Route,Redirect} from 'react-router-dom';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import {COMMENTS}  from '../shared/comments'
-import {PROMOTIONS}  from '../shared/promotions'
-import {LEADERS}  from '../shared/leaders'
+import {COMMENTS}  from '../shared/comments';
+import {PROMOTIONS}  from '../shared/promotions';
+import {LEADERS}  from '../shared/leaders';
 class Main extends Component {
   constructor(props){
     super(props);
@@ -17,17 +17,17 @@ class Main extends Component {
       dishes : DISHES,
       comments: COMMENTS,
       promotions : PROMOTIONS,
-      leaders: LEADERS
+      leaders: LEADERS,
+      
     };
   }
   render(){
     const HomePage = () => {
       return(
-        <Home 
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
               promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
               leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
+        />
       )
     }
     return (
