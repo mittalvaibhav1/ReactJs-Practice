e7d62e1007a84cf2f2119e7b9e2b24009f4d2c7c
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-09-09 14:35:51 +0530

    Comments Submission.

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,74 +1,9 @@
-import React , {Component} from 'react';
-import { Card, CardBody, CardTitle, CardImg, CardText, BreadcrumbItem, Breadcrumb } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { Row, Col, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
-import { Control, Errors, LocalForm } from 'react-redux-form';
+import React, { Component } from 'react'
+import {Row,Col, Button, Modal, ModalHeader, ModalBody,Label } from 'reactstrap';
+import {Control,Errors,LocalForm} from 'react-redux-form';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
-
-
-function RenderDish({ dish }) {
-    return (
-        <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg width="100%" object src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>
-    )
-}
-function RenderComments({ comments }) {
-    const comment = comments.map((comment) => {
-        return (
-            <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-            </li>
-        )
-    })
-    return (
-        <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <ul className="list-unstyled">
-                {comment}
-            </ul>
-            <CommentForm />
-        </div>
-    )
-}
-
-const DishDetail = (props) => {
-    if (props.dish != null) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div></div>
-        )
-    }
-
-}
 class CommentForm extends Component {
     constructor(props) {
         super(props);
@@ -98,7 +33,7 @@ class CommentForm extends Component {
                         <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Col md={12}>
-                                    <Label htmlFor="rating">Rating</Label>
+                                <Label htmlFor="rating">Rating</Label>
                                     <Control.select model=".rating" name="Rating"
                                         className="form-control">
                                         <option>1</option>
@@ -112,9 +47,9 @@ class CommentForm extends Component {
                             <Row className="form-group">
                                 <Col>
                                     <Label htmlFor="name">Your Name</Label>
-                                    <Control.text model=".name" name="name" id="name" placeholder="Your Name" className="form-control" validators={{
-                                        required, minLength: minLength(3), maxLength: maxLength(15)
-                                    }} />
+                                    <Control.text model=".name" name="name" id="name" placeholder="Your Name" className="form-control"  validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} />
                                     <Errors
                                         className="text-danger"
                                         model=".name"
@@ -124,12 +59,12 @@ class CommentForm extends Component {
                                             minLength: 'Must be greater than 2 characters',
                                             maxLength: 'Must be 15 characters or less'
                                         }}
-                                    />
+                                     />     
                                 </Col>
                             </Row>
                             <Row className="form-group">
                                 <Col md={12}>
-                                    <Label htmlFor="message">Comment</Label>
+                                <Label htmlFor="message">Comment</Label>
                                     <Control.textarea model=".message" id="message" name="message"
                                         rows="6"
                                         className="form-control" />
@@ -147,5 +82,4 @@ class CommentForm extends Component {
     }
 }
 
-
-export default DishDetail;
+export default CommentForm;
