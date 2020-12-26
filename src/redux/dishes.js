12fe3f8aeff333daa8d57264a5ce709a7d097233
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-27 03:32:40 +0530

    dishes reducer

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,6 +1,28 @@
-
-export const Dishes =(state=DISHES,action) => {
+import * as ActionTypes from './ActionTypes';
+export const Dishes = (state = {
+        isLoading : true,
+        errMess: null,
+        dishes: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {
+                isLoading : false,
+                dishes : payload,
+                errMess : null
+            }
+        case ActionTypes.DISHES_LOADING:
+            return {
+                isLoading : true,
+                dishes : [],
+                errMess : null
+            }
+        case ActionTypes.DISHES_FAILED:
+            return {
+                isLoading : false,
+                dishes : [],
+                errMess : payload
+            }
         default :
             return state;
     }
