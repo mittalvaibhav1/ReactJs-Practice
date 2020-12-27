35efd4636ec7c8bf2d3cb40ca4cd697779fc229a
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-28 02:20:46 +0530

    Controlled React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
@@ -19,7 +20,8 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes : () => dispatch(fetchDishes())
+  fetchDishes : () => dispatch(fetchDishes()),
+  resetFeedbackForm: () => dispatch(actions.reset('feedback'))
 });
 
 class Main extends Component {
@@ -52,7 +54,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/contactus' component={() => <Contact resetFeedbackForm = { this.props.resetFeedbackForm } />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
