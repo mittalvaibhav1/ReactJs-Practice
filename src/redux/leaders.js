88d093859bb8092345ab496090057586532bcbe8
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-29 20:44:57 +0530

    leaders reducer

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,6 +1,28 @@
-import { LEADERS } from "../shared/leaders";
-export const Leaders = (state = LEADERS, action) => {
+import * as ActionTypes from './ActionTypes';
+
+export const Leaders = (state = {
+        isLoading : true,
+        leaders : [],
+        errMess : null
+    }, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_LEADERS :
+            return {
+                isLoading : false,
+                leaders : action.payload,
+                errMess : null
+            }
+        case ActionTypes.LEADERS_FAILED :
+            return {
+                isLoading : false,
+                leaders : [],
+                errMess : action.payload
+            }
+        case ActionTypes.LEADERS_LOADING :
+            return {
+                ...state,
+                isLoading : true
+            }         
         default:
             return state;
     }
