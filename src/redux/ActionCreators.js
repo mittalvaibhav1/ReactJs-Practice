54487d94875e760b935550b558394f1d8feeacd0
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-29 20:39:56 +0530

    leaders action creators

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -142,3 +142,38 @@ export const addPromos = (promos) => ({
     type : ActionTypes.ADD_PROMOS,
     payload : promos
 });
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading());
+    return fetch(baseUrl + 'leaders')
+        .then((response) => {
+            if(response.ok) {
+                return response;
+            }
+            else {
+                let error = new Error("Error: " + response.status + " " + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        (error) => {
+            throw error;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message))); 
+}
+
+export const leadersLoading = () => ({
+    type : ActionTypes.LEADERS_LOADING
+})
+
+export const addLeaders = (leaders) => ({
+    type : ActionTypes.ADD_LEADERS,
+    payload : leaders
+})
+
+export const leadersFailed = (error) => ({
+    type : ActionTypes.LEADERS_FAILED,
+    payload : error
+})
